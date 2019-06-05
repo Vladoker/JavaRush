ff2066265f214b224cd98e23e5ac376df29c6dbc
Vladislav <v5v_95@mail.ru>
2019-06-05 04:22:53 +0300

    create block try catch

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task18.task1803;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 /* 
 Самые частые байты
@@ -8,5 +10,10 @@ import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Куку");
+        } catch (Exception ex) {
+
+        }
     }
 }
