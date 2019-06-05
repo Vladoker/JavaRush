bf36c740710ef1d539ee87b714e974b3c70e19aa
Vladislav <v5v_95@mail.ru>
2019-06-05 04:46:00 +0300

    create array

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task18.task1803;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /* 
 Самые частые байты
@@ -11,9 +12,7 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.println("Куку");
-            System.out.println("Куку");
-            System.out.println("Куку");
+            ArrayList list = new ArrayList();
         } catch (Exception ex) {
 
         }
