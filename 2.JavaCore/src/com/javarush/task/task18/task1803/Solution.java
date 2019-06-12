6727c48353ddbb0d1c016ccabbce01e5006224d4
Vladislav <v5v_95@mail.ru>
2019-06-12 04:12:19 +0300

    Самые частые байты

--- 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -1,9 +1,12 @@
 package com.javarush.task.task18.task1803;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /* 
 Самые частые байты
@@ -12,7 +15,30 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) throws Exception {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            ArrayList list = new ArrayList();
+            Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(reader.readLine()));
+
+            int b = -1;
+
+            while((b = bufferedInputStream.read()) != -1){
+
+                if (map.containsKey(b)) {
+                    map.put(b, map.get(b) + 1);
+                }
+                else map.put(b, 1);
+            }
+            bufferedInputStream.close();
+            reader.close();
+            int maxValueInMap = Collections.max(map.values());
+
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (entry.getValue() == maxValueInMap) {
+                    System.out.print(entry.getKey() + " ");
+                }
+            }
+
+
         } catch (Exception ex) {
 
         }
