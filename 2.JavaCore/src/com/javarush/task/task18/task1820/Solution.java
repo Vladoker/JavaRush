c6451a2ac055c0ab2bbaed6a8a58777147efe832
Vladislav_HomePc <v5v_95@mail.ru>
2019-08-06 20:15:36 +0300

    test beta

--- 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
@@ -7,15 +7,20 @@ package com.javarush.task.task18.task1820;
 import java.io.*;
 import java.util.ArrayList;
 
-public class Solution {
+public class Solution  {
     public static void main(String[] args) {
 
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             BufferedInputStream inputstream = new BufferedInputStream(new FileInputStream(reader.readLine()));
             BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(reader.readLine()))
         ){
+            String name = reader.readLine();
+
             int b = -1;
             ArrayList<Integer> list = new ArrayList<>(inputstream.available());
+
+
+
 
             while ((b = inputstream.read()) != -1) {
                 if (b == 32)continue;
@@ -26,6 +31,7 @@ public class Solution {
         catch (Exception ex){
 
         }
+
 
     }
 }
