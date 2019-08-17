2ff67e4bdefef6c5fd4ef45f8ee1785e814b30d6
Vladislav_HomePc <v5v_95@mail.ru>
2019-08-17 14:49:05 +0300

    Знакомство с Java: переменные,
    Знакомство с Java: переменные, методы, классы

---
+++ 1.JavaSyntax/src/task02/task0207/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task02.task0207;
+
+/* 
+Кусочек калькулятора
+*/
+public class Solution {
+    public static void main(String[] args) {
+       int a=2,b=2;
+       System.out.println(a+b);//напишите тут ваш код
+       System.out.println(a*b);
+    }
+}
---
+++ 1.JavaSyntax/src/task02/task0208/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task02.task0208;
+
+/* 
+Одного кота нам мало
+*/
+public class Solution {
+    public static void main(String[] args) {
+     Cat proska = new Cat();
+     Cat fabi = new Cat();//напишите тут ваш код
+    }
+
+    public static class Cat {
+
+    }
+}
