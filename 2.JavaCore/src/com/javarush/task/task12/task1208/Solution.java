91ca2b1e9a93b90c97e89f75f3d7709edf93af1a
Vladislav_HomePc <v5v_95@mail.ru>
2019-08-17 14:28:08 +0300

    Основы ООП
    Основы ООП: перегрузка, полиморфизм, абстракция, интерфейсы.

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int print(int n) { return n; }
+    public static Integer print(Integer n) { return n; }
+
+    public static double print(double n) { return n; }
+    public static Double print(Double n) { return n; }
+
+    public static String print(String str) { return str; }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+public interface CanFly { String getFly();}
+public interface CanRun{ String getRun();}
+public interface CanSwim { String getSwim();}
+
+}
